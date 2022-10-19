8d189a43ce5e4518242116c42453e798d538baf5
Padmashree Jha <codewithpom@gmail.com>
2022-10-19 21:35:46 +0530

    Final DONE!!!!!!!

--- functions/getMeme.js
+++ functions/getMeme.js
@@ -4,6 +4,22 @@ const memes = require("../data/data.json")
 exports.handler = async function (event, context) {
     const request_body = JSON.parse(event.body)
     console.log(request_body)
+    const index = request_body['index']
+    const result = memes[index]
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            title: result.title,
+            url: url,
+            twitter_hashtags: result.twitter_hashtags,
+            index: index
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*',
+        },
+    };
 }
 
 
